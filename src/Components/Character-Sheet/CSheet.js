13fbd77cb0e8c5c9553e42e79a8e8a5ebe75e9b3
Harlan Heath <harlandheath@gmail.com>
2018-09-29 16:43:11 -0500

    character stats

---
+++ src/Components/Character-Sheet/CSheet.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class CSheet extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stats: []
+    };
+  }
+  render() {
+    return <div />;
+  }
+}
