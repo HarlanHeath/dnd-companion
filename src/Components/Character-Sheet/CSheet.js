7b79ccb7c6bcc7d22b1cd74ff6f68ad1f475364d
Harlan Heath <harlandheath@gmail.com>
2018-09-29 17:08:13 -0500

    state updated with character stats

--- src/Components/Character-Sheet/CSheet.js
+++ src/Components/Character-Sheet/CSheet.js
@@ -4,7 +4,14 @@ export default class CSheet extends Component {
   constructor() {
     super();
     this.state = {
-      stats: []
+      stats: [
+        { strength: 0 },
+        { dexterity: 0 },
+        { constitution: 0 },
+        { charisma: 0 },
+        { wisdom: 0 },
+        { intelligence: 0 }
+      ]
     };
   }
   render() {
